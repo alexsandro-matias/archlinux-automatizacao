fcb01723b06631017e753f75bb14335d9e917133
Alexsandro Matias de Almeida <matiasalexsandro@gmail.com>
2022-03-24 12:03:41 -0200

    Update script00_espelhos.sh
    Corrigindo a sincronização da hora do sistema operacional

--- script00_espelhos.sh
+++ script00_espelhos.sh
@@ -1,6 +1,11 @@
 
 # Atualizando as chaves 
-sudo rm -rf /etc/pacman.d/gnupg && sudo pacman-key --init ; sudo pacman-key --populate archlinux; sudo pacman -Syyu archlinux-keyring
+sudo hwclock -w
+sudo pacman -Scc
+sudo rm -rf /etc/pacman.d/gnupg
+sudo pacman-key --init
+sudo pacman-key --populate archlinux
+sudo pacman -Syyu archlinux-keyring
 
 # Atualizando o Sistema
 sudo pacman -Syyuu --noconfirm
