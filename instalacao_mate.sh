c013f6803cff92938f162e01dc96e9ce7e70c687
alexsandro-matias <matiasalexsandro@gmail.com>
2022-04-11 10:14:52 -0300

    excluindo o Mate

--- instalacao_mate.sh
+++ instalacao_mate.sh
@@ -2,4 +2,4 @@ sudo pacman -S mate mate-terminal lxdm --noconfirm
 
 # Configurando Gerenciador de Sessão
 sudo systemctl enable lxdm.service 
-sudo systemctl start lxdm.service 
+sudo systemctl start lxdm.service   204  sudo pacman -S mate-terminal 
---
+++ remocao_mate.sh
@@ -0,0 +1,5 @@
+sudo pacman -Rdd mate mate-terminal lxdm --noconfirm
+
+
+sh remocao_pacotes_desnecessarios.sh
+
