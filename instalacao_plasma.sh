a3296ca4e915798d940e13c295ca5096ff827c5d
alexsandro-matias <matiasalexsandro@gmail.com>
2022-05-07 21:46:14 -0300

    melhorada a instalação do plasma

--- instalacao_plasma.sh
+++ instalacao_plasma.sh
@@ -3,11 +3,4 @@ sudo pacman -Syyuu --noconfirm
 sudo pacman -S --noconfirm plasma sddm okular ark spectacle dolphin-plugins konsole kate kwrite
 
 
-sudo systemclt disable lxdm
-sudo systemclt stop lxdm
-
-sudo systemctl enable sddm
-sudo systemctl start sddm
-
-
 sh script04_remocao.sh
